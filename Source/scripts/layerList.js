d08d6ba1ef5c27da899e4865699f89c9d084a293
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2011-04-06 23:11:02 +0000

    * Added opacity sliders to layerList jQuery plugin.

--- Source/scripts/layerList.js
+++ Source/scripts/layerList.js
@@ -1,9 +1,12 @@
 ﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5-vsdoc.js "/>
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.11/jquery-ui.js"/>
 /*global jQuery, dojo */
 /*jslint browser: true, es5: true, undef: true, nomen: true, regexp: true, plusplus: true, bitwise: true, newcap: true, strict: true, maxerr: 500, indent: 4 */
 
 (function ($) {
-    /// <summary>Creates a list of layers for a map.  The "this" keyword is the DOM element that will be turned into the layer list.</summary>
+    /// <summary>
+    /// Creates a list of layers for a map.  The "this" keyword is the jQuery object containing the DOM element(s) that will be turned into a layer list.
+    /// </summary>
     $.fn.layerList = function (map) {
         var layerListNode = this;
         this.addClass("ui-esri-layer-list");
@@ -14,7 +17,19 @@
                 // Create a checkbox and label and place inside of a div.
                 var checkBox = $("<input>").attr("type", "checkbox").attr("data-layerId", layer.id);
                 var label = $("<label>").text(layer.id);
-                var layerDiv = $("<div>").attr("data-layerId", layer.id).append(checkBox).append(label);
+                // Create an opacity slider for the layer.
+                var opacitySlider = $("<div>").slider({
+                    min: 0.0,
+                    max: 1.0,
+                    step: 0.1,
+                    value: 1.0,
+                    disabled: !layer.visible,
+                    change: function (event, ui) {
+                        layer.setOpacity(ui.value);
+                    }
+                });
+                var layerDiv = $("<div>").attr("data-layerId", layer.id).append(checkBox).append(label).append(opacitySlider);
+
 
                 // Add the div to the document.
                 this.append(layerDiv);
@@ -27,6 +42,7 @@
                 // Make the checkbox turn the layer on and off.
                 checkBox.click(function (eventObject) {
                     layer.setVisibility(checkBox.attr("checked"));
+                    opacitySlider.slider({ disabled: !checkBox.attr("checked") });
                 });
 
 
