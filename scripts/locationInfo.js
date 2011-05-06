6ab9659ee219cb8a662b1bf6acbc727691cee00e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2011-05-06 21:23:30 +0000

    * Controls created by locationInfo.js are now dijits.

--- scripts/locationInfo.js
+++ scripts/locationInfo.js
@@ -5,6 +5,12 @@
 
 (function ($) {
     "use strict";
+
+    dojo.require("dijit.form.Button");
+    dojo.require("dijit.form.CheckBox");
+    dojo.require("dijit.form.NumberSpinner");
+    dojo.require("dijit.form.Select");
+
     $.fn.locationInfo = function (layerListUrl) {
         /// <summary>Creates the location information UI.</summary>
         /// <param name="layerListUrl" type="String">URL for the layer list REST endpoint.</param>
@@ -16,66 +22,69 @@
             // Set up an object for temprorarily holding DOM nodes as they are created.  Once added to "uiNode" they can be deleted from the nodes object.
             var nodes = {};
             nodes.bufferControl = $("<div>");
-            nodes.bufferControl.addClass("wsdot-location-info-buffer").append("<label>Buffer</label>");
+            nodes.bufferControl.addClass("wsdot-location-info-buffer").append("<label style='padding-right: 5px'>Buffer</label>");
 
-            nodes.bufferValue = $("<input type='number' value='0' min='0' class='wsdot-location-info-buffer-size'>");
+            nodes.bufferValue = $("<input type='number' value='0' min='0' id='wsdot-location-info-buffer-size'>");
             nodes.bufferControl.append(nodes.bufferValue);
+            dijit.form.NumberSpinner({ value: 0, constraints: { min: 0 }, style: "width: 50px" }, nodes.bufferValue[0]);
 
             // These are all of the measurement units for the buffer.
             var units = [
-                { "name": "Foot", "value": 9002, "description": "Feet" },
-                { "name": "InternationalInch", "value": 109008, "description": "Inches" },
-                { "name": "Meter", "value": 9001, "description": "Meters" },
-                { "name": "Kilometer", "value": 9036, "description": "Kilometers" },
-                { "name": "Decimeter", "value": 109005, "description": "Decimeters" },
-                { "name": "Centimeter", "value": 109006, "description": "Centimeters" },
-                { "name": "Millimeter", "value": 109007, "description": "Millimeters" },
-                { "name": "FiftyKilometerLength", "value": 109030, "description": "50 Kilometer Length" },
-                { "name": "OneHundredFiftyKilometerLength", "value": 109031, "description": "150 Kilometer Length" },
-                { "name": "GermanMeter", "value": 9031, "description": "German legal meter" },
-                { "name": "SurveyFoot", "value": 9003, "description": "US survey foot" },
-                { "name": "ClarkeFoot", "value": 9005, "description": "Clarke's foot" },
-                { "name": "Fathom", "value": 9014, "description": "Fathom" },
-                { "name": "NauticalMile", "value": 9030, "description": "Nautical Miles" },
-                { "name": "SurveyChain", "value": 9033, "description": "US survey chain" },
-                { "name": "SurveyLink", "value": 9034, "description": "US survey link" },
-                { "name": "SurveyMile", "value": 9035, "description": "US survey mile" },
-                { "name": "ClarkeYard", "value": 9037, "description": "Yard (Clarke's ratio)" },
-                { "name": "ClarkeChain", "value": 9038, "description": "Chain (Clarke's ratio)" },
-                { "name": "ClarkeLink", "value": 9039, "description": "Link (Clarke's ratio)" },
-                { "name": "SearsYard", "value": 9040, "description": "Yard (Sears)" },
-                { "name": "SearsFoot", "value": 9041, "description": "Sears' foot" },
-                { "name": "SearsChain", "value": 9042, "description": "Chain (Sears)" },
-                { "name": "SearsLink", "value": 9043, "description": "Link (Sears)" },
-                { "name": "Benoit1895A_Yard", "value": 9050, "description": "Yard (Benoit 1895 A)" },
-                { "name": "Benoit1895A_Foot", "value": 9051, "description": "Foot (Benoit 1895 A)" },
-                { "name": "Benoit1895A_Chain", "value": 9052, "description": "Chain (Benoit 1895 A)" },
-                { "name": "Benoit1895A_Link", "value": 9053, "description": "Link (Benoit 1895 A)" },
-                { "name": "Benoit1895B_Yard", "value": 9060, "description": "Yard (Benoit 1895 B)" },
-                { "name": "Benoit1895B_Foot", "value": 9061, "description": "Foot (Benoit 1895 B)" },
-                { "name": "Benoit1895B_Chain", "value": 9062, "description": "Chain (Benoit 1895 B)" },
-                { "name": "Benoit1895B_Link", "value": 9063, "description": "Link (Benoit 1895 B)" },
-                { "name": "IndianFoot", "value": 9080, "description": "Indian geodetic foot" },
-                { "name": "Indian1937Foot", "value": 9081, "description": "Indian foot (1937)" },
-                { "name": "Indian1962Foot", "value": 9082, "description": "Indian foot (1962)" },
-                { "name": "Indian1975Foot", "value": 9083, "description": "Indian foot (1975)" },
-                { "name": "IndianYard", "value": 9084, "description": "Indian yard" },
-                { "name": "Indian1937Yard", "value": 9085, "description": "Indian yard (1937)" },
-                { "name": "Indian1962Yard", "value": 9086, "description": "Indian yard (1962)" },
-                { "name": "Indian1975Yard", "value": 9087, "description": "Indian yard (1975)" },
-                { "name": "Foot1865", "value": 9070, "description": "Foot (1865)" },
-                { "name": "British1936Foot", "value": 9095, "description": "British Foot (1936)" },
-                { "name": "GoldCoastFoot", "value": 9094, "description": "Gold Coast Foot" },
-                { "name": "InternationalChain", "value": 109003, "description": "International Chain" },
-                { "name": "InternationalLink", "value": 109004, "description": "International Link" },
-                { "name": "InternationalYard", "value": 109001, "description": "Yards" },
-                { "name": "StatuteMile", "value": 9093, "description": "Miles" },
-                { "name": "SurveyYard", "value": 109002, "description": "US survey Yard" },
-                { "name": "USsurveyInch", "value": 109009, "description": "US survey inch" },
-                { "name": "InternationalRod", "value": 109010, "description": "International rod" },
-                { "name": "USsurveyRod", "value": 109011, "description": "US survey rod" },
-                { "name": "USNauticalMile", "value": 109012, "description": "US nautical mile (pre-1954)" },
-                { "name": "UKNauticalMile", "value": 109013, "description": "UK nautical mile (pre-1970)" }
+                { "name": "Foot", "value": 9002, "description": "ft." },
+                { "name": "InternationalInch", "value": 109008, "description": "in." },
+                { "name": "Meter", "value": 9001, "description": "m." },
+                { "name": "Kilometer", "value": 9036, "description": "km." },
+                { "name": "Decimeter", "value": 109005, "description": "dm." },
+                { "name": "Centimeter", "value": 109006, "description": "cm." },
+                { "name": "Millimeter", "value": 109007, "description": "mm." }
+            /*
+            ,{ "name": "FiftyKilometerLength", "value": 109030, "description": "50 Kilometer Length" },
+            { "name": "OneHundredFiftyKilometerLength", "value": 109031, "description": "150 Kilometer Length" },
+            { "name": "GermanMeter", "value": 9031, "description": "German legal meter" },
+            { "name": "SurveyFoot", "value": 9003, "description": "US survey foot" },
+            { "name": "ClarkeFoot", "value": 9005, "description": "Clarke's foot" },
+            { "name": "Fathom", "value": 9014, "description": "Fathom" },
+            { "name": "NauticalMile", "value": 9030, "description": "Nautical Miles" },
+            { "name": "SurveyChain", "value": 9033, "description": "US survey chain" },
+            { "name": "SurveyLink", "value": 9034, "description": "US survey link" },
+            { "name": "SurveyMile", "value": 9035, "description": "US survey mile" },
+            { "name": "ClarkeYard", "value": 9037, "description": "Yard (Clarke's ratio)" },
+            { "name": "ClarkeChain", "value": 9038, "description": "Chain (Clarke's ratio)" },
+            { "name": "ClarkeLink", "value": 9039, "description": "Link (Clarke's ratio)" },
+            { "name": "SearsYard", "value": 9040, "description": "Yard (Sears)" },
+            { "name": "SearsFoot", "value": 9041, "description": "Sears' foot" },
+            { "name": "SearsChain", "value": 9042, "description": "Chain (Sears)" },
+            { "name": "SearsLink", "value": 9043, "description": "Link (Sears)" },
+            { "name": "Benoit1895A_Yard", "value": 9050, "description": "Yard (Benoit 1895 A)" },
+            { "name": "Benoit1895A_Foot", "value": 9051, "description": "Foot (Benoit 1895 A)" },
+            { "name": "Benoit1895A_Chain", "value": 9052, "description": "Chain (Benoit 1895 A)" },
+            { "name": "Benoit1895A_Link", "value": 9053, "description": "Link (Benoit 1895 A)" },
+            { "name": "Benoit1895B_Yard", "value": 9060, "description": "Yard (Benoit 1895 B)" },
+            { "name": "Benoit1895B_Foot", "value": 9061, "description": "Foot (Benoit 1895 B)" },
+            { "name": "Benoit1895B_Chain", "value": 9062, "description": "Chain (Benoit 1895 B)" },
+            { "name": "Benoit1895B_Link", "value": 9063, "description": "Link (Benoit 1895 B)" },
+            { "name": "IndianFoot", "value": 9080, "description": "Indian geodetic foot" },
+            { "name": "Indian1937Foot", "value": 9081, "description": "Indian foot (1937)" },
+            { "name": "Indian1962Foot", "value": 9082, "description": "Indian foot (1962)" },
+            { "name": "Indian1975Foot", "value": 9083, "description": "Indian foot (1975)" },
+            { "name": "IndianYard", "value": 9084, "description": "Indian yard" },
+            { "name": "Indian1937Yard", "value": 9085, "description": "Indian yard (1937)" },
+            { "name": "Indian1962Yard", "value": 9086, "description": "Indian yard (1962)" },
+            { "name": "Indian1975Yard", "value": 9087, "description": "Indian yard (1975)" },
+            { "name": "Foot1865", "value": 9070, "description": "Foot (1865)" },
+            { "name": "British1936Foot", "value": 9095, "description": "British Foot (1936)" },
+            { "name": "GoldCoastFoot", "value": 9094, "description": "Gold Coast Foot" },
+            { "name": "InternationalChain", "value": 109003, "description": "International Chain" },
+            { "name": "InternationalLink", "value": 109004, "description": "International Link" },
+            { "name": "InternationalYard", "value": 109001, "description": "Yards" },
+            { "name": "StatuteMile", "value": 9093, "description": "Miles" },
+            { "name": "SurveyYard", "value": 109002, "description": "US survey Yard" },
+            { "name": "USsurveyInch", "value": 109009, "description": "US survey inch" },
+            { "name": "InternationalRod", "value": 109010, "description": "International rod" },
+            { "name": "USsurveyRod", "value": 109011, "description": "US survey rod" },
+            { "name": "USNauticalMile", "value": 109012, "description": "US nautical mile (pre-1954)" },
+            { "name": "UKNauticalMile", "value": 109013, "description": "UK nautical mile (pre-1970)" }
+            */
             ];
 
             nodes.bufferUnitSelect = $("<select id='wsdot-location-info-buffer-unit-select'>");
@@ -84,7 +93,7 @@
                 nodes.bufferUnitSelect.append("<option value='" + value.name + "' data-wkid='" + value.value + "'>" + value.description + "</option>");
             });
             nodes.bufferControl.append(nodes.bufferUnitSelect);
-
+            dijit.form.Select(null, nodes.bufferUnitSelect[0]);
             uiNode.append(nodes.bufferControl);
 
 
@@ -112,22 +121,30 @@
             $.each(data, function (index, layer) {
                 nodes.row = $("<tr>");
                 nodes.checkbox = $("<input type='checkbox'>");
-                // nodes.checkbox.attr("data-layer", JSON.stringify(layer));
-                nodes.checkbox.attr("data-UniqueId", layer.UniqueId);
+                //nodes.checkbox.attr("data-UniqueId", layer.UniqueId);
+                nodes.checkbox.attr("id", "wsdot-location-info-layer-" + layer.UniqueId);
                 nodes.td = $("<td>");
                 nodes.td.append(nodes.checkbox);
                 nodes.row.append(nodes.td);
                 nodes.row.append("<td>" + layer.LayerName + "</td>");
                 nodes.button = $("<button>Metadata</button>");
-                if (layer.Metadata) {
-                    nodes.button.attr("data-Metadata", layer.Metadata);
-                } else {
-                    nodes.button.attr("disabled", true);
-                }
+
                 nodes.td = $("<td class='metadata'>");
                 nodes.td.append(nodes.button);
                 nodes.td.appendTo(nodes.row);
                 nodes.tbody.append(nodes.row);
+
+                dijit.form.CheckBox({
+                    value: layer.UniqueId
+                }, nodes.checkbox[0]);
+
+                nodes.button = dijit.form.Button({ label: "Metadata", id: "wsdot-location-info-metadata-button-" + layer.UniqueId }, nodes.button[0]);
+                if (layer.Metadata) {
+                    nodes.button.attr("data-Metadata", layer.Metadata);
+                } else {
+                    nodes.button.set("disabled", true);
+                }
+
             });
 
             uiNode.append(nodes.dataSetsTable);
@@ -138,6 +155,10 @@
 
             nodes.shapeButtons.append("<button id='wsdot-location-info-point' >Point</button>").append("<button id='wsdot-location-info-polyline'>Polyline</button>").append("<button id='wsdot-location-info-polygon'>Polygon</button>");
             uiNode.append(nodes.shapeButtons);
+
+            dijit.form.Button({ label: "Point" }, "wsdot-location-info-point");
+            dijit.form.Button({ label: "Line" }, "wsdot-location-info-polyline");
+            dijit.form.Button({ label: "Polygon" }, "wsdot-location-info-polygon");
         }
 
         esri.request({
@@ -148,4 +169,4 @@
             error: function (error) { if (console && console.error) { console.error(error); } }
         }, { useProxy: true, usePost: false });
     };
-}(jQuery));
+} (jQuery));
