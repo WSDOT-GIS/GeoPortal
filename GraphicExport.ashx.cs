fec815ad018589a5fc8d073aea0b6f5f5ccfd90c
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2011-07-14 19:41:36 +0000

    * Began working on adding attributes to placemarks in exported KML.

--- GraphicExport.ashx.cs
+++ GraphicExport.ashx.cs
@@ -6,6 +6,7 @@ using Google.KML;
 using System.Web.Script.Serialization;
 using System.Collections;
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Wsdot.Grdo.Web.Mapping
 {
@@ -14,7 +15,7 @@ namespace Wsdot.Grdo.Web.Mapping
     /// </summary>
     public class GraphicExport : IHttpHandler
     {
-
+        private static readonly Regex _geometryInNameRe = new Regex("(?in)Geometry$");
         public void ProcessRequest(HttpContext context)
         {
             // Get the JSON string representing the saved graphics from the cookies.
@@ -48,13 +49,15 @@ namespace Wsdot.Grdo.Web.Mapping
                 format = string.Empty;
             }
 
-            if (Regex.IsMatch(format, "(?i)km[lz]")) //string.Compare(format, "kml", true) == 0)
+            if (Regex.IsMatch(format, "(?i)km[lz]"))
             {
+                // Generate a KML document.
                 var kmlDocument = new geDocument();
                 var jsSerializer = new JavaScriptSerializer();
 
                 // Loop through the layers.
                 var layers = jsSerializer.Deserialize<Dictionary<string, object>>(json);
+
 
                 foreach (var kvp in layers)
                 {
@@ -65,68 +68,20 @@ namespace Wsdot.Grdo.Web.Mapping
                     {
                         var placemark = new gePlacemark();
                         folder.Features.Add(placemark);
-                        var geometry = graphic["geometry"] as Dictionary<string, object>;
-                        if (geometry.Keys.Contains("x"))
+                        placemark.Geometry = JsonToKmlGeometry(graphic["geometry"] as Dictionary<string, object>);
+                        var attributesJson = (Dictionary<string, object>)graphic["attributes"];
+                        if (attributesJson.Keys.Contains("BufferedGeometry") && (decimal)attributesJson["BufferSize"] > 0)
                         {
-                            // Create point geometry
-                            // x and y are decimals;
-                            var x = Convert.ToDouble(geometry["x"]);
-                            var y = Convert.ToDouble(geometry["y"]);
-                            placemark.Geometry = new gePoint(new geCoordinates(new geAngle90(y), new geAngle180(x)));
+                            // TODO: Set the placemark geometry to a multi-geometry containing both the main and buffered geometries.
                         }
-                        else if (geometry.Keys.Contains("rings"))
-                        {
-                            // Create polygon geometry
-                            var rings = (ArrayList)geometry["rings"];
 
-                            var linearRings = from ArrayList ring in rings
-                                              select new geLinearRing((from ArrayList point in ring
-                                                                       select new geCoordinates(
-                                                                           new geAngle90(Convert.ToDouble(point[1])),
-                                                                           new geAngle180(Convert.ToDouble(point[0]))
-                                                                        )).ToList());
-                            if (linearRings.Count() > 1)
-                            {
-                                var polygon = new gePolygon(new geOuterBoundaryIs(linearRings.ElementAt(0)));
-                                polygon.InnerBoundaries.AddRange(from ring in linearRings.Skip(1)
-                                                                 select new geInnerBoundaryIs(ring));
-                                placemark.Geometry = polygon;
-                            }
-                            else
-                            {
-                                placemark.Geometry = linearRings.First();
-                            }
-                        }
-                        else if (geometry.Keys.Contains("paths"))
-                        {
-                            // Create line geometry
-                            var paths = (ArrayList)geometry["paths"];
-
-                            var lineStrings = from ArrayList ring in paths
-                                              select new geLineString((from ArrayList point in ring
-                                                                       select new geCoordinates(
-                                                                           new geAngle90(Convert.ToDouble(point[1])),
-                                                                           new geAngle180(Convert.ToDouble(point[0]))
-                                                                        )).ToList());
-                            if (lineStrings.Count() > 1)
-                            {
-                                var multiGeo = new geMultiGeometry();
-                                multiGeo.Geometries.AddRange(lineStrings);
-                                placemark.Geometry = multiGeo;
-                            }
-                            else
-                            {
-                                placemark.Geometry = lineStrings.First();
-                            }
-                            
-                        }                        
                     }
                 }
 
                 var kml = new geKML(kmlDocument);
 
+                // Export the geKML into either KML or KMZ, depending on the specified format.
                 byte[] bytes;
-
                 if (string.Compare(format, "kmz", true) == 0)
                 {
                     bytes = kml.ToKMZ();
@@ -147,6 +102,131 @@ namespace Wsdot.Grdo.Web.Mapping
                 context.Response.AddHeader("Content-Disposition", "filename=ExportedGraphics.json.txt");
                 context.Response.Write(json);
             }
+        }
+
+        private static geGeometry JsonToKmlGeometry(Dictionary<string, object> geometry)
+        {
+            geGeometry placemarkGeometry;
+            if (geometry.Keys.Contains("x"))
+            {
+                // Create point geometry
+                // x and y are decimals;
+                var x = Convert.ToDouble(geometry["x"]);
+                var y = Convert.ToDouble(geometry["y"]);
+                placemarkGeometry = new gePoint(new geCoordinates(new geAngle90(y), new geAngle180(x)));
+            }
+            else if (geometry.Keys.Contains("rings"))
+            {
+                // Create polygon geometry
+                var rings = (ArrayList)geometry["rings"];
+
+                var linearRings = from ArrayList ring in rings
+                                  select new geLinearRing((from ArrayList point in ring
+                                                           select new geCoordinates(
+                                                               new geAngle90(Convert.ToDouble(point[1])),
+                                                               new geAngle180(Convert.ToDouble(point[0]))
+                                                            )).ToList());
+                if (linearRings.Count() > 1)
+                {
+                    var polygon = new gePolygon(new geOuterBoundaryIs(linearRings.ElementAt(0)));
+                    polygon.InnerBoundaries.AddRange(from ring in linearRings.Skip(1)
+                                                     select new geInnerBoundaryIs(ring));
+                    placemarkGeometry = polygon;
+                }
+                else
+                {
+                    placemarkGeometry = linearRings.First();
+                }
+            }
+            else if (geometry.Keys.Contains("paths"))
+            {
+                // Create line geometry
+                var paths = (ArrayList)geometry["paths"];
+
+                var lineStrings = from ArrayList ring in paths
+                                  select new geLineString((from ArrayList point in ring
+                                                           select new geCoordinates(
+                                                               new geAngle90(Convert.ToDouble(point[1])),
+                                                               new geAngle180(Convert.ToDouble(point[0]))
+                                                            )).ToList());
+                if (lineStrings.Count() > 1)
+                {
+                    var multiGeo = new geMultiGeometry();
+                    multiGeo.Geometries.AddRange(lineStrings);
+                    placemarkGeometry = multiGeo;
+                }
+                else
+                {
+                    placemarkGeometry = lineStrings.First();
+                }
+
+            }
+            else
+            {
+                placemarkGeometry = null;
+            }
+            return placemarkGeometry;
+        }
+
+        private static string ToHtmlDL(Dictionary<string, object> attributes)
+        {
+            var descriptionBuilder = new StringBuilder("<dl>");
+            foreach (var attribute in attributes)
+            {
+                if (_geometryInNameRe.IsMatch(attribute.Key))
+                {
+                    // Ignore geometry fields.
+                    continue;
+                }
+                else if (string.Compare("QueryResults", attribute.Key, StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    descriptionBuilder.Append("<dt>Query Results</dt>");
+                    descriptionBuilder.Append("<dd>");
+                    var queryResults = (ArrayList)attribute.Value;
+                    foreach (Dictionary<string, object> qr in queryResults)
+                    {
+                        var layerInfo = qr["LayerInfo"] as Dictionary<string, object>;
+                        var resultTable = ((ArrayList)qr["ResultTable"]).Cast<Dictionary<string, object>>();
+                    }
+                    descriptionBuilder.Append("</dd>");
+                }
+            }
+
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts a list of dictionaries to an HTML table.  The keys of the dictionaries are assumed to be identical.
+        /// </summary>
+        /// <param name="attributes"></param>
+        /// <returns></returns>
+        private static string ToHtmlTable(IEnumerable<Dictionary<string, object>> attributes)
+        {
+            StringBuilder builder = new StringBuilder("<table>");
+            string[] keys = attributes.First(o => o != null).Select(o => o.Key).ToArray();
+            builder.Append("<thead><tr>");
+            foreach (var key in keys)
+            {
+                builder.AppendFormat("<th>{0}</th>", key);
+            }
+            builder.Append("</tr></thead>");
+
+            builder.Append("<tbody>");
+
+            foreach (var attr in attributes)
+            {
+                builder.Append("<tr>");
+                foreach (var key in keys)
+                {
+                    builder.AppendFormat("<td>{0}</td>", key);
+                }
+                builder.Append("</tr>");
+            }
+            builder.Append("</tbody></table>");
+
+
+            return builder.ToString();
         }
 
         public bool IsReusable
