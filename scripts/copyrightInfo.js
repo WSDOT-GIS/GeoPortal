2e5d1c1a92f01f1c702243ec6ebd95e022ca1f87
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2012-04-02 23:29:40 +0000

    * Simplified the method that copyright messages are added / removed from copyrightInfo widget.

--- scripts/copyrightInfo.js
+++ scripts/copyrightInfo.js
@@ -8,44 +8,57 @@
 		},
 		_onLayerAddResultLink: null,
 		_visibilityChangeLinks: [],
-		_addCopyright: function (/**{esri.layers.Layer}*/layer) {
-			$("<div>", this.element).attr({
-				"data-layerId": layer.id
-			}).text(layer.copyright).appendTo(this.element);
-		},
-		_removeCopyright: function (/**{esri.layers.Layer}*/layer) {
-			$("div[data-layerId=" + layer.id + "]", this.element).remove();
+		_list: null,
+		updateCopyrights: function () {
+			var i, l, layer, layerId, map = this.options.map;
+			// Clear existing copyright notices.
+			this._list.empty();
+			for (i = 0, l = map.layerIds.length; i < l; i++) {
+				layerId = map.layerIds[i];
+				layer = map.getLayer(layerId);
+				if (typeof (layer.copyright) !== "undefined") {
+					$("<li>").appendTo(this._list).text(layer.copyright);
+				}
+			}
+
+			if ($("> *", this._list).length === 0) {
+				$(this.element).hide();
+			} else {
+				$(this.element).show();
+			}
 		},
 		_create: function () {
 			var $this = this, layerAddHandler, layerRemoveHandler, visibiltiyChangeHandler;
 
 			$this.element.addClass("ui-copyrightInfo");
 
+			$this._list = $("<ul>").appendTo($this.element);
+
 			/** this {esri.layers.Layer} */
 			visibiltiyChangeHandler = function ( /**{Boolean}*/visibility) {
-				if (visibility) {
-					$this._addCopyright(this);
-				} else {
-					$this._removeCopyright(this);
-				}
+				$this.updateCopyrights();
 			};
 
 			layerAddHandler = function (/** {esri.layers.Layer} */layer, /** {Error} */error) {
 				var link;
 
+				if (error) {
+					return;
+				}
+
 				if (layer.visible) {
-					$this._addCopyright(layer);
+					$this.updateCopyrights();
 				}
 
 				// If the layer contains copyright information, setup an "onVisibilityChange" event handler.
-				if (typeof (layer.copyright) == "string" && layer.copyright.trim().length > 0) {
-					link = dojo.connect(layer, "onVisibilityChange", layer, visibiltiyChangeHandler);
-					$this._visibilityChangeLinks.push(link);
-				}
+
+				link = dojo.connect(layer, "onVisibilityChange", layer, visibiltiyChangeHandler);
+				$this._visibilityChangeLinks.push(link);
+
 			};
 
 			layerRemoveHandler = function (/**{esri.layers.Layer}*/layer) {
-				$this._removeCopyright(layer);
+				$this.updateCopyrights();
 			};
 
 			if ($this.options.map) {
