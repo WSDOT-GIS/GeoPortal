13dc1bfe740c8bbe927fd256cb3f6de1651d5377
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2012-06-11 18:19:28 +0000

    zoomToXY tool now accepts DMS coordinates (in addition to decimal degrees).

--- scripts/zoomToXY.js
+++ scripts/zoomToXY.js
@@ -9,8 +9,36 @@
 	*/
 
 	// Matches DMS coordinates
-	// http://regexpal.com/?flags=gim&regex=^%28-%3F\d%2B%28%3F%3A\.\d%2B%29%3F%29[%C2%B0%3Ad]%3F\s%3F%28\d%2B%28%3F%3A\.\d%2B%29%3F[%27%E2%80%B2%3A]%3F\s%3F%28\d%2B%28%3F%3A\.\d%2B%29%3F[%22%E2%80%B3]%3F%29%3F%29%3F\s%3F%28[NSEW]%29%3F&input=40%3A26%3A46N%2C79%3A56%3A55W%0A40%3A26%3A46.302N%2079%3A56%3A55.903W%0A40%C2%B026%E2%80%B247%E2%80%B3N%2079%C2%B058%E2%80%B236%E2%80%B3W%0A40d%2026%E2%80%B2%2047%E2%80%B3%20N%2079d%2058%E2%80%B2%2036%E2%80%B3%20W%0A40.446195N%2079.948862W%0A40.446195%2C%20-79.948862%0A40%C2%B0%2026.7717%2C%20-79%C2%B0%2056.93172%0A
-	var dmsRe = /(-?\d+(?:\.\d+)?)[°:d]?\s?(\d+(?:\.\d+)?['′:]?\s?(\d+(?:\.\d+)?["″]?)?)?\s?([NSEW])?/i
+	// http://regexpal.com/?flags=gim&regex=^%28-%3F\d%2B%28%3F%3A\.\d%2B%29%3F%29[%C2%B0%3Ad]%3F\s%3F%28%3F%3A%28\d%2B%28%3F%3A\.\d%2B%29%3F%29[%27%E2%80%B2%3A]%3F\s%3F%28\d%2B%28%3F%3A\.\d%2B%29%3F[%22%E2%80%B3]%3F%29%3F%29%3F\s%3F%28[NSEW]%29%3F&input=40%3A26%3A46N%2C79%3A56%3A55W%0A40%3A26%3A46.302N%2079%3A56%3A55.903W%0A40%C2%B026%E2%80%B247%E2%80%B3N%2079%C2%B058%E2%80%B236%E2%80%B3W%0A40d%2026%E2%80%B2%2047%E2%80%B3%20N%2079d%2058%E2%80%B2%2036%E2%80%B3%20W%0A40.446195N%2079.948862W%0A40.446195%2C%20-79.948862%0A40%C2%B0%2026.7717%2C%20-79%C2%B0%2056.93172%0A
+	var dmsRe = /^(-?\d+(?:\.\d+)?)[°:d]?\s?(?:(\d+(?:\.\d+)?)['′:]?\s?(?:(\d+(?:\.\d+)?)["″]?)?)?\s?([NSEW])?/i;
+	// Results of match will be [full coords string, Degrees, minutes (if any), seconds (if any), hemisphere (if any)]
+	// E.g., ["40:26:46.302N", "40", "26", "46.302", "N"]
+	// E.g., ["40.446195N", "40.446195", undefined, undefined, "N"]
+
+	/** Parses a Degrees Minutes Seconds string into a Decimal Degrees number.
+	* @param {string}  dmsStr
+	* @return {Number}
+	*/
+	function parseDms(dmsStr) {
+		var output = NaN, dmsMatch, degrees, minutes, seconds, hemisphere;
+		dmsMatch = dmsRe.exec(dmsStr);
+		if (dmsMatch) {
+			degrees = Number(dmsMatch[1]);
+
+			minutes = typeof (dmsMatch[2]) !== "undefined" ? Number(dmsMatch[2]) / 60 : 0;
+			seconds = typeof (dmsMatch[3]) !== "undefined" ? Number(dmsMatch[3]) / 3600 : 0;
+			hemisphere = dmsMatch[4] || null;
+			if (hemisphere !== null && /[SW]/i.test(hemisphere)) {
+				degrees = degrees * -1;
+			}
+			if (degrees < 0) {
+				output = degrees - minutes - seconds;
+			} else {
+				output = degrees + minutes + seconds;
+			}
+		}
+		return output;
+	}
 
 	$.widget("ui.zoomToXY", {
 		options: {
@@ -48,24 +76,7 @@
 		_create: function () {
 			var $this = this;
 
-			/** Converts a string to a number.  
-			* Differs from the Number function in that undefined, null, and strings that are empty or contain only whitespace return NaN instead of 0.
-			* @param {string} str A string representation of a number.  If a number is provided instead of a string, the same value will simply be returned.
-			*/
-			function toNumber(str) {
-				var type = typeof (str), output;
-				if (type === "string") {
-					if (typeof (str.trim) === "function") {
-						str = str.trim();
-					}
-					output = str.length < 1 ? NaN : Number(str);
-				} else if (type === "undefined" || str === null) {
-					output = NaN;
-				} else {
-					output = Number(str);
-				}
-				return output;
-			}
+
 
 			/** Adds a point to the map at the location specified in the widget's controls.
 			*/
@@ -76,8 +87,8 @@
 				x = $this._xBox.attr("value");
 				y = $this._yBox.attr("value");
 
-				x = toNumber(x);
-				y = toNumber(y);
+				x = parseDms(x);
+				y = parseDms(y);
 
 				// Check to make sure that the user put numbers into the text boxes.  (If the browser supports the HTML5 number type input, the boxes will not allow non-number values.)
 				if (!isNaN(x) && !isNaN(y)) {
