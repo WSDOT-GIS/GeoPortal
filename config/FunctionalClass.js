13d29ae897e10c9e5411a9e4b6e30ff9eb9d3661
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2013-02-11 17:17:01 +0000

    Corrected lod levels to start at 0 instead of 1.

--- config/FunctionalClass.js
+++ config/FunctionalClass.js
@@ -20,19 +20,19 @@
 			}
 		},
 		"lods": [
-				{ "level": 1, "resolution": 1222.99245256249, "scale": 4622324.434309 },
-				{ "level": 2, "resolution": 611.49622628138, "scale": 2311162.217155 },
-				{ "level": 3, "resolution": 305.748113140558, "scale": 1155581.108577 },
-				{ "level": 4, "resolution": 152.874056570411, "scale": 577790.554289 },
-				{ "level": 5, "resolution": 76.4370282850732, "scale": 288895.277144 },
-				{ "level": 6, "resolution": 38.2185141425366, "scale": 144447.638572 },
-				{ "level": 7, "resolution": 19.1092570712683, "scale": 72223.819286 },
-				{ "level": 8, "resolution": 9.55462853563415, "scale": 36111.909643 },
-				{ "level": 9, "resolution": 4.77731426794937, "scale": 18055.954822 },
-				{ "level": 10, "resolution": 2.38865713397468, "scale": 9027.977411 },
-				{ "level": 11, "resolution": 1.19432856685505, "scale": 4513.988705 },
-				{ "level": 12, "resolution": 0.597164283559817, "scale": 2256.994353 },
-				{ "level": 13, "resolution": 0.298582141647617, "scale": 1128.497176 }
+				{ "level": 0, "resolution": 1222.99245256249, "scale": 4622324.434309 },
+				{ "level": 1, "resolution": 611.49622628138, "scale": 2311162.217155 },
+				{ "level": 2, "resolution": 305.748113140558, "scale": 1155581.108577 },
+				{ "level": 3, "resolution": 152.874056570411, "scale": 577790.554289 },
+				{ "level": 4, "resolution": 76.4370282850732, "scale": 288895.277144 },
+				{ "level": 5, "resolution": 38.2185141425366, "scale": 144447.638572 },
+				{ "level": 6, "resolution": 19.1092570712683, "scale": 72223.819286 },
+				{ "level": 7, "resolution": 9.55462853563415, "scale": 36111.909643 },
+				{ "level": 8, "resolution": 4.77731426794937, "scale": 18055.954822 },
+				{ "level": 9, "resolution": 2.38865713397468, "scale": 9027.977411 },
+				{ "level": 10, "resolution": 1.19432856685505, "scale": 4513.988705 },
+				{ "level": 11, "resolution": 0.597164283559817, "scale": 2256.994353 },
+				{ "level": 12, "resolution": 0.298582141647617, "scale": 1128.497176 }
 		],
 		"sliderStyle": "large"
 	},
