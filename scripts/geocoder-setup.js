55920bf376ee0e23195b03656dc84ce1ba1292a0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-03-13 23:12:30 +0000

    Moved geocoder setup into separate module.

---
+++ scripts/geocoder-setup.js
@@ -0,0 +1,127 @@
+﻿// Create the Geocoder widget.
+define([
+	"dojo/_base/Color",
+	"esri/layers/GraphicsLayer",
+	"esri/dijit/Geocoder",
+	"esri/symbols/SimpleMarkerSymbol",
+	"esri/renderers/ClassBreaksRenderer",
+	"esri/InfoTemplate",
+	"dojo/_base/array"
+], function (Color, GraphicsLayer, Geocoder, SimpleMarkerSymbol, ClassBreaksRenderer, InfoTemplate, array) {
+	var geocoder, graphicsLayer;
+
+	/**
+		* Creates the graphics layer that the geocoder will use to show results.
+		* @returns {GraphicsLayer}
+		*/
+	function createGraphicsLayer() {
+		var renderer, layer, infos;
+		renderer = new ClassBreaksRenderer(new SimpleMarkerSymbol(), "score");
+		infos = [
+		{
+			minValue: 100,
+			maxValue: 101,
+			symbol: new SimpleMarkerSymbol().setColor(new Color("#00FF00")),
+			label: "100",
+			description: "Perfect Match"
+		},
+		{
+			minValue: 90,
+			maxValue: 100,
+			symbol: new SimpleMarkerSymbol().setColor(new Color("#B0FF00")),
+			label: "High",
+			description: "High Score"
+		},
+		{
+			minValue: 80,
+			maxValue: 90,
+			symbol: new SimpleMarkerSymbol().setColor(new Color("#FFFF00")),
+			label: "Medium-High",
+			description: "Medium-High Score"
+		},
+		{
+			minValue: 70,
+			maxValue: 80,
+			symbol: new SimpleMarkerSymbol().setColor(new Color("#FF7D16")),
+			label: "Medium",
+			description: "Medium Score"
+		},
+		{
+			minValue: 0,
+			maxValue: 69,
+			symbol: new SimpleMarkerSymbol().setColor(new Color("#FF0000")),
+			label: "Low",
+			description: "Low Score"
+		}
+
+		];
+		array.forEach(infos, function (info) {
+			renderer.addBreak(info);
+		});
+
+		layer = new GraphicsLayer({
+			id: "Located Addresses",
+			className: "address-graphics-layer",
+			dataAttributes: ["score", "Addr_Type"]
+		});
+		layer.setInfoTemplate(new InfoTemplate("Address", "<p>${name}</p><p>Score: ${Score}</p>"));
+		layer.setRenderer(renderer);
+
+		return layer;
+	}
+
+	function setupGeocoder(map, node) {
+		graphicsLayer = createGraphicsLayer();
+		map.addLayer(graphicsLayer);
+
+		
+		geocoder = new Geocoder({
+			arcgisGeocoder: {
+				sourceCountry: "US",
+				searchExtent: wsdot.config.mapOptions.extent,
+				placeholder: "Find an address"
+			},
+			autoComplete: true,
+			autoNavigate: false,
+			map: map
+		}, node);
+		geocoder.startup();
+		geocoder.focus();
+
+
+		/** @typedef {Object} GeocodeResult
+			* @property {string} name - The returned address string.
+			* @property {Extent} extent
+			* @property {Graphic} feature - Attributes include Addr_Type (e.g., "StreetAddress") and Score (e.g., 100).
+			*/
+
+		/** @typedef {Object} GeocodeResponse
+			* @property {GeocodeResult} result
+			* @property {Geocoder} target
+			*/
+
+		/** Handles the response from a Geocode operation.
+			* @param {GeocodeResult} response
+			*/
+		function handleResults(response) {
+			var feature;
+			if (response.result) {
+				feature = response.result.feature;
+				if (feature) {
+					feature.attributes.name = response.result.name;
+					graphicsLayer.add(feature);
+					map.infoWindow.setFeatures([feature]);
+					map.infoWindow.show(feature.geometry);
+				}
+			}
+		}
+
+		geocoder.on("find-results", handleResults);
+		geocoder.on("select", handleResults);
+
+		return geocoder;
+	}
+
+	return setupGeocoder;
+
+});
--- scripts/index.js
+++ scripts/index.js
@@ -972,126 +972,12 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 					// Address Search
 					toolsAccordion.addChild(new ContentPane({ title: "Find an Address" }, domConstruct.create("div", { id: "searchTools" }, "toolsAccordion")));
 					on.once(registry.byId("searchTools"), "show", function () {
-						// Create the Geocoder widget.
-						require([
-							"dojo/_base/Color",
-							"esri/dijit/Geocoder",
-							"esri/symbols/SimpleMarkerSymbol",
-							"esri/renderers/ClassBreaksRenderer",
-							"esri/InfoTemplate"
-						], function (Color, Geocoder, SimpleMarkerSymbol, ClassBreaksRenderer, InfoTemplate) {
-							var div, geocoder, graphicsLayer;
-
-							/**
-							 * Creates the graphics layer that the geocoder will use to show results.
-							 * @returns {GraphicsLayer}
-							 */
-							function createGraphicsLayer() {
-								var renderer, layer, infos;
-								renderer = new ClassBreaksRenderer(new SimpleMarkerSymbol(), "score");
-								infos = [
-								{
-									minValue: 100,
-									maxValue: 101,
-									symbol: new SimpleMarkerSymbol().setColor(new Color("#00FF00")),
-									label: "100",
-									description: "Perfect Match"
-								},
-								{
-									minValue: 90,
-									maxValue: 100,
-									symbol: new SimpleMarkerSymbol().setColor(new Color("#B0FF00")),
-									label: "High",
-									description: "High Score"
-								},
-								{
-									minValue: 80,
-									maxValue: 90,
-									symbol: new SimpleMarkerSymbol().setColor(new Color("#FFFF00")),
-									label: "Medium-High",
-									description: "Medium-High Score"
-								},
-								{
-									minValue: 70,
-									maxValue: 80,
-									symbol: new SimpleMarkerSymbol().setColor(new Color("#FF7D16")),
-									label: "Medium",
-									description: "Medium Score"
-								},
-								{
-									minValue: 0,
-									maxValue: 69,
-									symbol: new SimpleMarkerSymbol().setColor(new Color("#FF0000")),
-									label: "Low",
-									description: "Low Score"
-								}
-
-								];
-								array.forEach(infos, function (info) {
-									renderer.addBreak(info);
-								});
-
-								layer = new GraphicsLayer({
-									id: "Located Addresses",
-									className: "address-graphics-layer",
-									dataAttributes: ["score", "Addr_Type"]
-								});
-								layer.setInfoTemplate(new InfoTemplate("Address", "<p>${name}</p><p>Score: ${Score}</p>"));
-								layer.setRenderer(renderer);
-
-								return layer;
-							}
-
-							graphicsLayer = createGraphicsLayer();
-							map.addLayer(graphicsLayer);
-
-							div = document.createElement("div");
+						require(["geocoder-setup"], function (setupGeocoder) {
+							var div = document.createElement("div");
 							div.id = "searchControl";
 							document.getElementById("searchTools").appendChild(div);
-							geocoder = new Geocoder({
-								arcgisGeocoder: {
-									sourceCountry: "US",
-									searchExtent: wsdot.config.mapOptions.extent,
-									placeholder: "Find an address"
-								},
-								autoComplete: true,
-								autoNavigate: false,
-								map: map
-							}, div);
-							geocoder.startup();
-							geocoder.focus();
-							
 
-							/** @typedef {Object} GeocodeResult
-							 * @property {string} name - The returned address string.
-							 * @property {Extent} extent
-							 * @property {Graphic} feature - Attributes include Addr_Type (e.g., "StreetAddress") and Score (e.g., 100).
-							 */
-
-							/** @typedef {Object} GeocodeResponse
-							 * @property {GeocodeResult} result
-							 * @property {Geocoder} target
-							 */
-
-							/** Handles the response from a Geocode operation.
-							 * @param {GeocodeResult} response
-							 */
-							function handleResults(response) {
-								var feature;
-								if (response.result) {
-									feature = response.result.feature;
-									if (feature) {
-										feature.attributes.name = response.result.name;
-										graphicsLayer.add(feature);
-										map.infoWindow.setFeatures([feature]);
-										map.infoWindow.show(feature.geometry);
-									}
-								}
-							}
-
-							geocoder.on("find-results", handleResults);
-							geocoder.on("select", handleResults);
-
+							setupGeocoder(map, div);
 						});
 					});
 				}
