85fd5340e27dab36e76fb94a90b5c6c994a41992
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-03-13 22:55:52 +0000

    When locating an address, a graphic is now added to the map and an info window is displayed.

--- scripts/index.js
+++ scripts/index.js
@@ -36,6 +36,7 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 	"esri/dijit/Legend",
 	"esri/layers/ArcGISTiledMapServiceLayer",
 	"esri/toolbars/navigation",
+	"esri/layers/GraphicsLayer",
 
 	"dijit/form/Button",
 	"dijit/layout/BorderContainer",
@@ -43,6 +44,7 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 	"dijit/layout/TabContainer",
 	"dijit/layout/AccordionContainer",
 	"dojox/layout/ExpandoPane",
+
 	"dijit/form/RadioButton",
 	"dijit/form/Select",
 	"dijit/form/FilteringSelect",
@@ -61,6 +63,7 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 	"esri/tasks/gp",
 	"esri/layers/FeatureLayer",
 	"esri/IdentityManager",
+
 	"extensions/esriApiExtensions",
 	"extensions/htmlPopupExtensions",
 	"extensions/metadataExtensions",
@@ -69,7 +72,7 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 	"extensions/map"
 ], function (require, ready, on, registry, array, number, dom, domAttr, domConstruct,
 	config, Map, jsonUtils, Point, Extent, GeometryService, Legend, ArcGISTiledMapServiceLayer, Navigation,
-	Button, BorderContainer, ContentPane, TabContainer, AccordionContainer, ExpandoPane) {
+	GraphicsLayer, Button, BorderContainer, ContentPane, TabContainer, AccordionContainer, ExpandoPane) {
 	"use strict";
 
 	var map = null, extents = null, navToolbar, createLinks = {}, defaultConfigUrl = "config/config.js";
@@ -970,19 +973,125 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry", "dojo/_base/array
 					toolsAccordion.addChild(new ContentPane({ title: "Find an Address" }, domConstruct.create("div", { id: "searchTools" }, "toolsAccordion")));
 					on.once(registry.byId("searchTools"), "show", function () {
 						// Create the Geocoder widget.
-						require(["esri/dijit/Geocoder"], function (Geocoder) {
-							var div, geocoder;
+						require([
+							"dojo/_base/Color",
+							"esri/dijit/Geocoder",
+							"esri/symbols/SimpleMarkerSymbol",
+							"esri/renderers/ClassBreaksRenderer",
+							"esri/InfoTemplate"
+						], function (Color, Geocoder, SimpleMarkerSymbol, ClassBreaksRenderer, InfoTemplate) {
+							var div, geocoder, graphicsLayer;
+
+							/**
+							 * Creates the graphics layer that the geocoder will use to show results.
+							 * @returns {GraphicsLayer}
+							 */
+							function createGraphicsLayer() {
+								var renderer, layer, infos;
+								renderer = new ClassBreaksRenderer(new SimpleMarkerSymbol(), "score");
+								infos = [
+								{
+									minValue: 100,
+									maxValue: 101,
+									symbol: new SimpleMarkerSymbol().setColor(new Color("#00FF00")),
+									label: "100",
+									description: "Perfect Match"
+								},
+								{
+									minValue: 90,
+									maxValue: 100,
+									symbol: new SimpleMarkerSymbol().setColor(new Color("#B0FF00")),
+									label: "High",
+									description: "High Score"
+								},
+								{
+									minValue: 80,
+									maxValue: 90,
+									symbol: new SimpleMarkerSymbol().setColor(new Color("#FFFF00")),
+									label: "Medium-High",
+									description: "Medium-High Score"
+								},
+								{
+									minValue: 70,
+									maxValue: 80,
+									symbol: new SimpleMarkerSymbol().setColor(new Color("#FF7D16")),
+									label: "Medium",
+									description: "Medium Score"
+								},
+								{
+									minValue: 0,
+									maxValue: 69,
+									symbol: new SimpleMarkerSymbol().setColor(new Color("#FF0000")),
+									label: "Low",
+									description: "Low Score"
+								}
+
+								];
+								array.forEach(infos, function (info) {
+									renderer.addBreak(info);
+								});
+
+								layer = new GraphicsLayer({
+									id: "Located Addresses",
+									className: "address-graphics-layer",
+									dataAttributes: ["score", "Addr_Type"]
+								});
+								layer.setInfoTemplate(new InfoTemplate("Address", "<p>${name}</p><p>Score: ${Score}</p>"));
+								layer.setRenderer(renderer);
+
+								return layer;
+							}
+
+							graphicsLayer = createGraphicsLayer();
+							map.addLayer(graphicsLayer);
+
 							div = document.createElement("div");
 							div.id = "searchControl";
 							document.getElementById("searchTools").appendChild(div);
 							geocoder = new Geocoder({
 								arcgisGeocoder: {
 									sourceCountry: "US",
-									searchExtent: wsdot.config.mapOptions.extent
+									searchExtent: wsdot.config.mapOptions.extent,
+									placeholder: "Find an address"
 								},
+								autoComplete: true,
+								autoNavigate: false,
 								map: map
 							}, div);
 							geocoder.startup();
+							geocoder.focus();
+							
+
+							/** @typedef {Object} GeocodeResult
+							 * @property {string} name - The returned address string.
+							 * @property {Extent} extent
+							 * @property {Graphic} feature - Attributes include Addr_Type (e.g., "StreetAddress") and Score (e.g., 100).
+							 */
+
+							/** @typedef {Object} GeocodeResponse
+							 * @property {GeocodeResult} result
+							 * @property {Geocoder} target
+							 */
+
+							/** Handles the response from a Geocode operation.
+							 * @param {GeocodeResult} response
+							 */
+							function handleResults(response) {
+								var feature;
+								if (response.result) {
+									feature = response.result.feature;
+									if (feature) {
+										feature.attributes.name = response.result.name;
+										graphicsLayer.add(feature);
+										map.infoWindow.setFeatures([feature]);
+										map.infoWindow.show(feature.geometry);
+									}
+								}
+							}
+
+							geocoder.on("find-results", handleResults);
+							geocoder.on("select", handleResults);
+
 						});
 					});
 				}
