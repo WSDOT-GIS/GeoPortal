5c0eaf70604751b0adbe8d887aff3f9b38bdec07
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-07-24 19:55:11 +0000

    Geocoder now centers the map when an address is located.

--- scripts/geocoder-setup.js
+++ scripts/geocoder-setup.js
@@ -6,82 +6,13 @@ define([
 	"esri/dijit/Geocoder",
 	"esri/symbols/SimpleMarkerSymbol",
 	"esri/renderers/ClassBreaksRenderer",
-	"esri/InfoTemplate",
-	"dojo/_base/array"
-], function (Color, GraphicsLayer, Geocoder, SimpleMarkerSymbol, ClassBreaksRenderer, InfoTemplate, array) {
-	var geocoder, graphicsLayer;
+	"esri/InfoTemplate"
+], function (Color, GraphicsLayer, Geocoder, SimpleMarkerSymbol, ClassBreaksRenderer, InfoTemplate) {
+	var geocoder, infoTemplate;
 
-	/**
-		* Creates the graphics layer that the geocoder will use to show results.
-		* @returns {GraphicsLayer}
-		*/
-	function createGraphicsLayer() {
-		var renderer, layer, infos;
-		renderer = new ClassBreaksRenderer(new SimpleMarkerSymbol(), "score");
-		infos = [
-		{
-			minValue: 100,
-			maxValue: 101,
-			symbol: new SimpleMarkerSymbol().setColor(new Color("#00FF00")),
-			label: "100",
-			description: "Perfect Match"
-		},
-		{
-			minValue: 90,
-			maxValue: 100,
-			symbol: new SimpleMarkerSymbol().setColor(new Color("#B0FF00")),
-			label: "High",
-			description: "High Score"
-		},
-		{
-			minValue: 80,
-			maxValue: 90,
-			symbol: new SimpleMarkerSymbol().setColor(new Color("#FFFF00")),
-			label: "Medium-High",
-			description: "Medium-High Score"
-		},
-		{
-			minValue: 70,
-			maxValue: 80,
-			symbol: new SimpleMarkerSymbol().setColor(new Color("#FF7D16")),
-			label: "Medium",
-			description: "Medium Score"
-		},
-		{
-			minValue: 0,
-			maxValue: 69,
-			symbol: new SimpleMarkerSymbol().setColor(new Color("#FF0000")),
-			label: "Low",
-			description: "Low Score"
-		}
-
-		];
-		array.forEach(infos, function (info) {
-			renderer.addBreak(info);
-		});
-
-		layer = new GraphicsLayer({
-			id: "Located Addresses",
-			className: "address-graphics-layer",
-			dataAttributes: ["score", "Addr_Type"]
-		});
-		layer.setInfoTemplate(new InfoTemplate("Address", "<p>${name}</p><p>Score: ${Score}</p>"));
-		layer.setRenderer(renderer);
-
-		return layer;
-	}
+	infoTemplate = new InfoTemplate("Address", "<p>${name}</p><p>Score: ${Score}</p>");
 
 	function setupGeocoder(map, node) {
-		graphicsLayer = createGraphicsLayer();
-		if (map.loaded) {
-			map.addLayer(graphicsLayer);
-		} else {
-			map.on("load", function () {
-				map.addLayer(graphicsLayer);
-			});
-		}
-
-		
 		geocoder = new Geocoder({
 			arcgisGeocoder: {
 				sourceCountry: "US",
@@ -115,9 +46,10 @@ define([
 				feature = response.result.feature;
 				if (feature) {
 					feature.attributes.name = response.result.name;
-					graphicsLayer.add(feature);
+					feature.setInfoTemplate(infoTemplate);
 					map.infoWindow.setFeatures([feature]);
 					map.infoWindow.show(feature.geometry);
+					map.centerAt(feature.geometry);
 				}
 			}
 		}
