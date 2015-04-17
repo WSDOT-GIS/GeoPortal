0e69893654ae54cf7f14a606d27e5f7e20c3d399
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-04-17 23:32:10 +0000

    Added restrictions to Search widget so that it only searches within USA and WA extent.

--- scripts/index.js
+++ scripts/index.js
@@ -62,6 +62,8 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry",
 	"extentSelect",
 	"geolocate-button",
 	"esri/dijit/Search",
+	"esri/tasks/locator",
+	"dojo/i18n!esri/nls/jsapi",
 
 	"dijit/form/RadioButton",
 	"dijit/form/Select",
@@ -94,7 +96,7 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry",
 	esriConfig, Map, jsonUtils, Point, Extent, GeometryService, Legend, ArcGISTiledMapServiceLayer, Navigation,
 	GraphicsLayer, HomeButton, Button, BorderContainer, ContentPane, TabContainer, AccordionContainer, ExpandoPane,
 	Scalebar, Graphic, webMercatorUtils, InfoTemplate, QueryTask, Query, BasemapGallery, BasemapLayer, SpatialReference,
-	Measurement, esriRequest, LabelLayer, SimpleRenderer, createExtentSelect, createGeolocateButton, Search
+	Measurement, esriRequest, LabelLayer, SimpleRenderer, createExtentSelect, createGeolocateButton, Search, Locator, i18n
 ) {
 	"use strict";
 
@@ -1090,11 +1092,25 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry",
 
 					var search = new Search({
 						map: map,
-					}, addressDiv);
+						enableHighlight: false,
+						sources: [
+							{
+								locator: new Locator("//geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"),
+								singleLineFieldName: "SingleLine",
+								outFields: ["Addr_type"],
+								name: i18n.widgets.Search.main.esriLocatorName,
+								localSearchOptions: {
+									minScale: 300000,
+									distance: 50000
+								},
+								placeholder: i18n.widgets.Search.main.placeholder,
+								//highlightSymbol: new PictureMarkerSymbol(this.basePath + "/images/search-pointer.png", 36, 36).setOffset(9, 18)
+								countryCode: "US",
+								searchExtent: extents.fullExtent
+							}
+						]
 
-					//var source = search.get("sources")[0];
-					//console.log("source", source);
-					//source.countryCode = "US";
+					}, addressDiv);
 
 					search.startup();
 				}
