20d05d1364cf51c1560c1c6a54ffbf7f8584c928
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-05-28 23:26:38 +0000

    Added specialization methods for queryStringHelper.js

--- scripts/queryStringHelper/queryStringHelper.js
+++ scripts/queryStringHelper/queryStringHelper.js
@@ -7,6 +7,49 @@ define(function () {
 	 */
 	var exports = {};
 
+	//var numberRe = /^-?\d+(?:\.\d+)?$/;
+
+	var extentRe = /\bextent=(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)/i;
+
+	/**
+	 * 
+	 * @param {string} [s=location.search]
+	 */
+	function getExtentValues(s) {
+		// Zoom to the extent in the query string (if provided).
+		// Test example:
+		// extent=-13677603.622831678,5956814.051290565,-13576171.686297385,6004663.630997022
+
+		if (!s) {
+			s = window.location.search;
+		}
+		var match = s.match(extentRe);
+		var coords;
+
+		if (match) {
+			match.splice(1, 4);
+			coords = match.map(function (s) {
+				return parseFloat(s);
+			});
+		}
+		return coords;
+	}
+
+	/**
+	 * Parses a key/value pair from a query string into an object.
+	 * @param {string} kvp
+	 */
+	function KeyValuePair(kvp) {
+		var arr = kvp.split("=");
+		var key = arr[0];
+		var value = arr.length >= 2 ? decodeURIComponent(arr[1]) : null;
+
+		/**@member {string} */
+		this.key = key;
+		/**@member {?string} */
+		this.value = value;
+	}
+
 	/**
 	 * Converts an object into a query string.
 	 * @returns {string}
@@ -23,19 +66,30 @@ define(function () {
 		return output.join("&");
 	};
 
+
+
 	/**
 	 * Converts a query string into an object.
+	 * @param {string} [search=window.location.search]
 	 * @returns {Object.<string, ?string>}
 	 */
-	exports.queryStringToObject = function (/**{string}*/ search) {
+	exports.queryStringToObject = function (search) {
 		var o = {}, kvPairs;
+		if (!search) {
+			search = window.location.search;
+		}
+		search = search.replace(/^\?/, "");
 		if (search) {
 			kvPairs = search.split("&");
 			kvPairs.forEach(function (s) {
-				var arr = s.split("=");
-				var key = arr[0];
-				var value = arr.length >= 2 ? decodeURIComponent(arr[1]) : null;
-				o[key] = value;
+				var kvp;
+
+				if (extentRe.test(s)) {
+					o.extent = getExtentValues();
+				} else {
+					kvp = new KeyValuePair(s);
+					o[kvp.key] = kvp.value;
+				}
 			});
 		}
 		return o;
