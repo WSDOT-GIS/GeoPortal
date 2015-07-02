aad3fb8a824f6eddab7893f01086a40c7b80446e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-07-02 00:49:23 +0000

    Removed disclaimer cookie. jquery.cookie.js is no longer used.

--- scripts/index.js
+++ scripts/index.js
@@ -228,38 +228,79 @@ require(["require", "dojo/ready", "dojo/on", "dijit/registry",
 
 		settingName = "GeoportalAggreedToDisclaimer" + configName;
 
+		var previousDisclaimerText = window.localStorage.getItem(settingName);
+
 		// Show the disclaimer if there is no cookie indicating that the user has seen it before.
-		if (wsdot.config.disclaimer !== undefined && (showEvenIfAlreadyAgreed || (wsdot.config.disclaimer !== null && !$.cookie(settingName)))) {
+		if (wsdot.config.disclaimer !== undefined && (showEvenIfAlreadyAgreed || (wsdot.config.disclaimer !== null))) {
+
+
+			var deferred = new Promise(function (resolve, reject) {
+				var request;
+				try {
+					request = new XMLHttpRequest();
+					request.open("get", wsdot.config.disclaimer);
+					request.onloadend = function (xEvt) {
+						
+						console.log(xEvt);
+						if (this.status === 200) {
+							if (!showEvenIfAlreadyAgreed && this.response === previousDisclaimerText) {
+								resolve(null);
+							} else {
+								resolve(this.response);
+							}
+						} else {
+							reject(this.statusText);
+						}
+					};
+					request.send();
+				} catch (err) {
+					reject(err);
+				}
+			});
+
+			deferred.then(function (disclaimerHtml) {
+				var parser;
+				console.log(disclaimerHtml);
+				var doc, title, body;
+				if (disclaimerHtml) {
+					parser = new DOMParser();
+					doc = parser.parseFromString(disclaimerHtml, "text/html");
+					title = doc.querySelector("head > title");
+					title = title.textContent;
+					body = doc.querySelector("body");
+					body = body.innerHTML;
+					$("<div>").html(body).dialog({
+						title: title.length > 0 ? title : "Disclaimer",
+						modal: true,
+						closeOnEscape: false,
+						width: 600,
+						buttons: {
+							"Accept": function () {
+								$(this).dialog("close");
+							}
+						},
+						open: function (/*event, ui*/) {
+							// Remove the close button from the disclaimer form.
+							var form = $(this).parent();
+							$("a.ui-dialog-titlebar-close", form).remove();
+						},
+						close: function (/*event, ui*/) {
+							// Store the current date with the setting.
+							window.localStorage.setItem(settingName, disclaimerHtml);
+							$(this).dialog("destroy").remove();
+						}
+					});
+
+
+				}
+			}, function (error) {
+				console.error("Error creating disclaimer", error);
+			});
+
+
 			// Load the content into a div.  Only when the source page has loaded do invoke the dialog constructor.
 			// This is to ensure that the dialog is centered on the page.
-			return $("<div>").load(wsdot.config.disclaimer, function () {
-				// Remove the title element.
-				var doc, title;
-				doc = $(this);
-				title = $("title", this).text();
-				$("title,link", this).remove();
-				doc.dialog({
-					title: title.length > 0 ? title : "Disclaimer",
-					modal: true,
-					closeOnEscape: false,
-					width: 600,
-					buttons: {
-						"Accept": function () {
-							$(this).dialog("close");
-						}
-					},
-					open: function (/*event, ui*/) {
-						// Remove the close button from the disclaimer form.
-						var form = $(this).parent();
-						$("a.ui-dialog-titlebar-close", form).remove();
-					},
-					close: function (/*event, ui*/) {
-						// Add a cookie
-						$.cookie(settingName, true, { expires: 5 });
-						$(this).dialog("destroy").remove();
-					}
-				});
-			});
+			
 		}
 	}
 
