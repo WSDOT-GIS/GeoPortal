163a50964c464a1c76535d923079aa546c8b1737
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-10-13 18:14:31 +0000

    Increased tolerance

--- scripts/extensions/htmlPopupExtensions.js
+++ scripts/extensions/htmlPopupExtensions.js
@@ -339,7 +339,7 @@ require([
                             // The following settings are configurable via the 'options' parameter.
                             idParams.returnGeometry = options.returnGeometry || true;
                             idParams.layerOption = options.layerOption || IdentifyParameters.LAYER_OPTION_ALL;
-                            idParams.tolerance = options.tolerance || 1;
+                            idParams.tolerance = options.tolerance || 5;
                             idParams.maxAllowableOffset = options.maxAllowableOffset || 5;
 
                             // Execute the identify task
@@ -636,7 +636,7 @@ require([
                 map.infoWindow.clearFeatures();
 
                 map.identify(event.mapPoint, {
-                    tolerance: 1
+                    tolerance: 5
                 }).then(function (idResults) {
                     //console.debug(idResults);
                     var results, features = [];
