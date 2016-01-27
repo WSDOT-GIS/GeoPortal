09c0d0257bc26a29ae3c459ff51bd3e93d839bc2
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-01-27 18:00:35 +0000

    Compatibility fixes and styling changes to PtrSites.

--- PtrSites/index.js
+++ PtrSites/index.js
@@ -11,30 +11,51 @@
  * @see {@link http://resources.arcgis.com/en/help/arcgis-rest-api/#/Query_Map_Service_Layer/02r3000000p1000000/|Query (Map Service\Layer)}
  */
 
-require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
+require(["esri/config", "esri/tasks/Geoprocessor"], function (esriConfig, Geoprocessor) {
     "use strict";
+
+    /**
+     * Converts date to YYYY-MM-DD format string.
+     * @param {Date} date - A Date.
+     * @returns {string} YYYY-MM-DD format representation of date.
+     */
+    function formatDate(date) {
+        return date ? date.toISOString().replace(/T.+$/, "") : null;
+    }
 
 
     var gpUrl = "http://hqolymgis99t:6080/arcgis/rest/services/Traffic/GetFilteredCsv/GPServer/Get Filtered CSV";
     var siteIdsUrl = "http://hqolymgis99t:6080/arcgis/rest/services/Traffic/PTRSites/MapServer/0/query?where=1%3D1&outFields=ADCTraffic.DBO.PTRSites.SiteID,ADCTraffic.DBO.ADCTrafficSiteCurrentLocation.SiteLocation&returnGeometry=false&orderByFields=ADCTraffic.DBO.PTRSites.SiteID&returnDistinctValues=true&f=json"
+    esriConfig.defaults.io.corsEnabledServers.push("hqolymgis99t:6080");
 
     /**
      * An object that manages a list item associated with a job.
      * @class
+     * @property {string} jopId - The identifier for the geoprocessing job.
      * @property {HTMLLIElement} listItem - The list item (<li>) associated with this object.
      * @property {Boolean} loading - Gets or sets the "loading" status of this object. Changing this value will toggle the "loading" class on the list item.
-     * @property {string} link - The URL inside of the <a> element.
+     * @property {string} link - Gets or sets the URL inside of the <a> element.
+     * @property {string} siteId - Gets or sets the site ID label on the link.
+     * @property {string} startDate - Gets or sets the start date label on the link.
+     * @property {string} endDate - Gets or sets the end date label on th elink.
+     * @property {string} error - Gets or sets the error message on the list item.
      */
     function JobListItem() {
         var _jobId = null;
         var li = document.createElement("li");
+        li.classList.add("list-group-item");
         li.classList.add("loading");
         var progress = document.createElement("progress");
         progress.textContent = "Loading...";
-        li.appendChild(progress);
+
+
         
         var a = document.createElement("a");
-        a.href = "";
+
+        var icon = document.createElement("span");
+        icon.setAttribute("class", "glyphicon glyphicon-compressed");
+        a.appendChild(icon);
+
         //a.innerHTML = "<span></span> from <span></span> to <span></span>"
         var siteIdSpan = document.createElement("span");
         var startDateSpan = document.createElement("span");
@@ -45,6 +66,7 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
         a.appendChild(document.createTextNode(" to "));
         a.appendChild(endDateSpan);
         li.appendChild(a);
+        li.appendChild(progress);
 
         var errorP = document.createElement("p");
         errorP.classList.add("error");
@@ -124,7 +146,7 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
     var url = new URL(window.location.href);
 
     // Get the GP parameters from the URL search if available.
-    var siteId = url.searchParams.get("siteid");
+    var siteId = url.searchParams.get("siteid") || "";
     var startDate = url.searchParams.get("startdate");
     var endDate = url.searchParams.get("enddate");
 
@@ -132,12 +154,14 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
     startDate = startDate ? new Date(startDate) : null;
     endDate = endDate ? new Date(endDate) : null;
 
+    var dateFmt = new Intl.DateTimeFormat();
+
 
     // Populate the form with the values from the URL search.
     var form = document.forms[0];
     form.siteid.setAttribute("value", siteId);
-    form.startdate.valueAsDate = startDate;
-    form.enddate.valueAsDate = endDate;
+    form.startdate.value = formatDate(startDate); //valueAsDate = startDate; // valueAsDate not currently (2016-01-27) supported in Firefox.
+    form.enddate.value = formatDate(endDate); //.valueAsDate = endDate;
 
     /**
      * @typedef {Object} GPMessage
@@ -168,15 +192,15 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
     function submitJob() {
         var params = {
             Site_ID: form.siteid.value,
-            Start_Date: form.startdate.valueAsDate,
-            End_Date: form.enddate.valueAsDate
+            Start_Date: form.startdate.valueAsDate || new Date(form.startdate.value),
+            End_Date: form.enddate.valueAsDate || new Date(form.enddate.value)
         };
 
+        // Update the URL search parameters.
         url.searchParams.set("siteid", form.siteid.value);
         url.searchParams.set("startdate", form.startdate.value);
         url.searchParams.set("enddate", form.enddate.value);
-
-        window.history.pushState(params, null, url.toString());
+        window.history.replaceState(params, null, url.toString());
 
         var li = new JobListItem();
         li.siteId = params.Site_ID;
@@ -202,6 +226,10 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
         submitJob();
         return false;
     };
+
+    form.onreset = function () {
+        history.replaceState(null, null, "./");
+    }
 
     function populateSiteIdOptionList() {
         // Populate site id option list.
@@ -232,7 +260,7 @@ require(["esri/tasks/Geoprocessor"], function (Geoprocessor) {
                 var siteId = feature.attributes[field];
                 var option = document.createElement("option");
                 option.value = siteId;
-                option.textContent = feature.attributes[descField];
+                option.textContent = [siteId, feature.attributes[descField] || "[No description]"].join(": ");
                 docFrag.appendChild(option);
             });
             siteIdList.appendChild(docFrag);
