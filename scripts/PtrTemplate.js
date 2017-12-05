97607272f3e75db376c8d34e553b7291f43990ca
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-12-04 16:03:28 -0800

    Updated PTR download URL from QA to Production

--- scripts/PtrTemplate.js
+++ scripts/PtrTemplate.js
@@ -9,7 +9,7 @@ define(["esri/InfoTemplate"], function (InfoTemplate) {
      * @alias module:PtrTemplate
      */
     return new InfoTemplate({
-        content: "${Site Location}<p><a href='http://webpub4qa.wsdot.wa.gov/Traffic/API/PermanentTrafficRecorder/?siteId=${Site ID}' target='_blank'>Download CSV</a></p>", //createContent,
+        content: "${Site Location}<p><a href='https://www.wsdot.wa.gov/Traffic/API/PermanentTrafficRecorder/?siteId=${Site ID}' target='_blank'>Download CSV</a></p>", //createContent,
         title: "PTR Site: ${Site ID}"
     });
 
