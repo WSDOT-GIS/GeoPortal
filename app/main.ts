e082517c60c0a9de4e8a41ff79173ce9929971ec
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-02-16 14:55:09 -0800

    Controls are now in Expands
    Controls are now in collapsable Expand elements

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,10 @@
 import Extent = require("esri/geometry/Extent");
-import Locator = require("esri/tasks/Locator");
 import FeatureLayer = require("esri/layers/FeatureLayer");
 import EsriMap = require("esri/Map");
+import Locator = require("esri/tasks/Locator");
 import MapView = require("esri/views/MapView");
+import BasemapGallery = require("esri/widgets/BasemapGallery");
+import Expand = require("esri/widgets/Expand");
 import LayerList = require("esri/widgets/LayerList");
 import Search = require("esri/widgets/Search");
 
@@ -13,6 +15,7 @@ const waExtent = new Extent({
   ymax: 49.05
 });
 
+// Create the map
 const map = new EsriMap({
   basemap: "streets-night-vector"
 });
@@ -23,38 +26,82 @@ const countyLayer = new FeatureLayer({
   url: countyLayerUrl
 });
 
+map.add(countyLayer);
+
+// Create the view.
 const view = new MapView({
   map,
   container: "viewDiv",
   extent: waExtent
 });
 
-const searchWidget = new Search({ view });
-// Restrict to US.
+// #region Add controls
+
+// Add Search
+const searchWidget = new Search({
+  view,
+  container: document.createElement("div")
+});
+
+// Modifiy the default World Locator:
+// * Restrict to US
+// * Restrict search extent to WA extent.
 const worldLocator = searchWidget.defaultSource as __esri.LocatorSource;
 worldLocator.countryCode = "US";
 worldLocator.filter = {
-    geometry: waExtent,
-    where: "1 = 1"
+  geometry: waExtent,
+  where: "1 = 1"
 } as __esri.LocatorSourceFilter;
 
-view.ui.add(searchWidget, {
-  position: "top-right",
-  index: 1
-});
-
-const layerList = new LayerList({
-  view
-});
-
-view.ui.add(layerList, {
-  position: "top-right",
-  index: 2
-});
-
-map.add(countyLayer);
-
+// Add the county Feature Layer as a search source.
 searchWidget.sources.add({
   featureLayer: countyLayer,
   name: "County Boundaries"
 } as __esri.FeatureLayerSource);
+
+// Place the search control into an expander.
+const searchExpand = new Expand({
+  content: searchWidget.container,
+  expandIconClass: "esri-icon-search",
+  view
+});
+
+// Add expander to the UI.
+view.ui.add(searchExpand, {
+  position: "top-right",
+  index: 1
+});
+
+// Create the layer list.
+const layerList = new LayerList({
+  container: document.createElement("div"),
+  view
+});
+
+// Add the layer list to an expander
+const layerListExpand = new Expand({
+  expandIconClass: "esri-icon-layer-list",
+  view,
+  content: layerList.container
+});
+
+// Add expander to the UI.
+view.ui.add(layerListExpand, {
+  position: "top-right",
+  index: 2
+});
+
+// Create basemap gallery, add to expander, add expander to view UI.
+const basemapGallery = new BasemapGallery({
+  container: document.createElement("div"),
+  view
+});
+
+const basemapExpand = new Expand({
+  expandIconClass: "esri-icon-basemap",
+  view,
+  content: basemapGallery.container
+});
+
+view.ui.add(basemapExpand, "top-right");
+// #endregion
