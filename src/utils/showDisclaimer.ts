9884b951f41166b8e173c91ddb8bcc94f7e38f2a
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-03-26 17:28:58 -0700

    refactor: modify disclaimer layout

--- src/utils/showDisclaimer.ts
+++ src/utils/showDisclaimer.ts
@@ -2,10 +2,9 @@
 
 /**
  * Shows the disclaimer dialog if the configuration contains a disclaimer.  If the dialog is shown, a jQuery object containing the dialog is returned.
- * @param {Boolean} showEvenIfAlreadyAgreed - Set this to true if you want to force the disclaimer to be shown even if there is a cookie indicating the user has already agreed.
- * @returns {Object}
+ * @param showEvenIfAlreadyAgreed - Set this to true if you want to force the disclaimer to be shown even if there is a cookie indicating the user has already agreed.
  */
-export default function showDisclaimer(
+export default async function showDisclaimer(
   disclaimerUrl: string,
   showEvenIfAlreadyAgreed?: boolean
 ) {
@@ -31,34 +30,52 @@ export default function showDisclaimer(
     disclaimerUrl !== undefined &&
     (showEvenIfAlreadyAgreed || disclaimerUrl !== null)
   ) {
-    (async () => {
-      const disclaimerHtml = await fetch(disclaimerUrl).then((response) =>
-        response.text()
-      );
+    const disclaimerHtml = await fetch(disclaimerUrl).then((response) =>
+      response.text()
+    );
 
-      if (disclaimerHtml && disclaimerHtml !== previousDisclaimerText) {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(disclaimerHtml, "text/html");
-        const titleElement = doc.querySelector("head > title");
-        const title = titleElement?.textContent ?? "Disclaimer";
-        const body = doc.querySelector("body")?.innerHTML;
+    if (disclaimerHtml && disclaimerHtml !== previousDisclaimerText) {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(disclaimerHtml, "text/html");
+      const titleElement = doc.querySelector("head > title");
+      const title = titleElement?.textContent ?? "Disclaimer";
+      const body = doc.querySelector("body")?.cloneNode(true)?.childNodes;
 
-        const innerHtml = `
-              <h1>${title}</h1>
-              ${body}
-                <form method="dialog">
-                    <input type="checkbox" id="doNotShowAgain" /><label for="doNotShowAgain">Do not show again</label>
-                    <button autofocus>OK</button>
-                </form>`;
+      const dialog = document.createElement("dialog");
+      dialog.classList.add("disclaimer-dialog");
 
-        const dialog = document.createElement("dialog");
-        dialog.innerHTML = innerHtml;
+      const h1 = document.createElement("h1");
+      h1.textContent = title;
+      dialog.appendChild(h1);
 
-        document.body.appendChild(dialog);
-
-        dialog.showModal();
+      if (body) {
+        dialog.append(...body);
       }
-    })();
+
+      const form = document.createElement("form");
+      form.method = "dialog";
+
+      if (!showEvenIfAlreadyAgreed) {
+        const doNotShowAgain = document.createElement("input");
+        doNotShowAgain.type = "checkbox";
+        doNotShowAgain.id = "doNotShowAgain";
+        const label = document.createElement("label");
+        label.htmlFor = "doNotShowAgain";
+        label.textContent = "Do not show again";
+        form.appendChild(doNotShowAgain);
+        form.appendChild(label);
+      }
+
+      const okButton = document.createElement("button");
+      okButton.type = "submit";
+      okButton.textContent = "OK";
+      okButton.autofocus = true;
+      form.appendChild(okButton);
+      dialog.appendChild(form);
+
+      document.body.appendChild(dialog);
+      dialog.showModal();
+    }
 
     // Load the content into a div.  Only when the source page has loaded do invoke the dialog constructor.
     // This is to ensure that the dialog is centered on the page.
